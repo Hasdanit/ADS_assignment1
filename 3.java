cd5b9e3086b5551f191dbcf821245ae7d566eed6
Hasdanit <153712498+Hasdanit@users.noreply.github.com>
2024-04-05 18:54:16 +0500

    Update 3.java

--- 3.java
+++ 3.java
@@ -1,1 +1,14 @@
-
+public class Main {
+    public static void main(String[] args) {
+      System.out.println(sumPositive(5));
+    }
+  
+    public static int sumPositive(int n){
+        if(n == 1){
+            return 1;
+        }
+        else{
+            return sumPositive(n-1) + n;
+        }
+    }
+  }
