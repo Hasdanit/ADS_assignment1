04470db9bf761f7664cab5b81a86ea374c939f9e
Hasdanit <153712498+Hasdanit@users.noreply.github.com>
2024-04-05 18:56:22 +0500

    Update 4.java

--- 4.java
+++ 4.java
@@ -1,1 +1,16 @@
+import java.math.*;
 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(sumPower(2, 3));
+    }
+  
+    public static double sumPower(int b, int n){
+        if (n == 0){
+            return Math.pow(b, n);
+        }
+        else{
+            return sumPower(b, n-1) + Math.pow(b,n);
+        }
+    }
+  }
