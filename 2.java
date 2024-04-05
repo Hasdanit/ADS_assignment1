6fe2331a22eee7f771790a27bacbb72081491225
Hasdanit <153712498+Hasdanit@users.noreply.github.com>
2024-04-05 18:53:01 +0500

    Update 2.java

--- 2.java
+++ 2.java
@@ -1,1 +1,15 @@
-
+public class Main {
+    public static void main(String[] args) {
+      int p[] = {0,1,2,3,4,5};
+        System.out.println(sumArray(p, 4));
+    }
+  
+    public static int sumArray(int[] arr, int n){
+        if (n == 0){
+            return arr[n];
+        }
+        else{
+            return sumArray(arr, n-1) + arr[n];
+        }
+    }
+  }
