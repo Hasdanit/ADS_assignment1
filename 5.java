62c9b33bee74a0d11570d08187d26a534c949f85
Hasdanit <153712498+Hasdanit@users.noreply.github.com>
2024-04-05 18:59:05 +0500

    Update 5.java

--- 5.java
+++ 5.java
@@ -1,1 +1,17 @@
+import java.math.*;
 
+public class Main {
+    public static void main(String[] args) {
+        printRSequence(5);
+    }
+  
+    public static void printRSequence(int n){
+        if(n == 0){
+            System.out.println(0);
+        }
+        else{
+            System.out.print(n + " ");
+            printRSequence(n-1);
+        }
+    }
+  }
