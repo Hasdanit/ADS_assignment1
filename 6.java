db6219da269d9f187652e14ef1b30f17c250bdae
Hasdanit <153712498+Hasdanit@users.noreply.github.com>
2024-04-05 19:01:22 +0500

    Update 6.java

--- 6.java
+++ 6.java
@@ -1,1 +1,18 @@
+import java.math.*;
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        printReversedStrings(n, scan);
+    }
+  
+    public static void printReversedStrings(int n, Scanner scan){
+        if (n > 0) {
+            String str = scan.nextLine();
+            printReversedStrings(n - 1, scan);
+            System.out.println(str);
+        }
+    }
+  }
