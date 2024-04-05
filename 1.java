c829f83a8e918cd345def0f0aa8f285bf7f02eb6
Hasdanit <153712498+Hasdanit@users.noreply.github.com>
2024-04-05 18:51:44 +0500

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,1 +1,12 @@
-
+public class Main {
+    public static void main(String[] args) {
+      System.out.println(sumSquares(5));
+    }
+  
+    public static int sumSquares(int n){
+            if(n == 1){
+                return 1;
+            }
+            return sumSquares(n-1) + n*n;
+    }
+  }
